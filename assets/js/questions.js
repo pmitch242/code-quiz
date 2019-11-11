90bd768767beb93a1dd9c1d8ca4c82bf1a918fac
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2019-11-11 05:11:57 -0500

    added more questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -9,6 +9,36 @@ var questions = [
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
     },
-    ///etc.
+    {
+      title: "JavaScript Code can be called by using____.",
+      choices: ["RMI", "Triggering Event", "Preprocessor", "Function/Method"],
+      answer: "Function/Method"
+    },
+    {
+      title: "JavaScript _________ when there is an indefinite or an infinite value during an arithmetic computation.",
+      choices: ["Prints an exception error", "Prints an overflow error", "Displays “Infinity”", "Prints the value as such"],
+      answer: "Displays “Infinity”"
+    },
+    {
+      title: "JavaScript is a _______________ language.",
+      choices: ["Object-Oriented", "High-level", "Assembly-language", "Object-Based"],
+      answer: "Object-Based"
+    },
+    {
+      title: "A conditional expression is also called a _______________",
+      choices: ["Alternative to if-else", "Immediate if", "If-then-else statement", "Switch statement"],
+      answer: "Immediate if"
+    },
+    {
+      title: "The pop() method of the array does which of the following task?",
+      choices: ["decrements the total length by 1", "increments the total length by 1", "prints the first element but no effect on the length", "updates the element"],
+      answer: "decrements the total length by 1"
+    },
+    {
+      title: "What will happen if reverse() and join() methods are used simultaneously?",
+      choices: ["Reverses and stores in the same array", "Reverses and concatenates the elements of the array", "Reverses", "Stores the elements of an array in normal order"],
+      answer: "Reverses and stores in the same array"
+    },
+
   ];
   
