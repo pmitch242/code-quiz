3d0207df5ef4a87522d2bf3f7c7d0f70d373d660
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2019-11-11 03:50:44 -0500

    added Names to scores display

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -17,14 +17,33 @@ backButton.textContent = "BACK";
 main.appendChild(backButton);
 
 scoreDisplay = localStorage.getItem("scores");
+initialsDisplay = localStorage.getItem("initials");
 scoreDisplay = JSON.parse(scoreDisplay);
+initialsDisplay = JSON.parse(initialsDisplay);
 if( scoreDisplay != null){
 
     for (i = 0; i < scoreDisplay.length; i++) {
+        //create Elements
         var scoreLi = document.createElement("li");
-        scoreLi.innerHTML = scoreDisplay[i];
-        scoreLi.setAttribute("class", "list-group-item text-left")
+        var rowDiv = document.createElement("div");
+        var col1 = document.createElement("div");
+        var col2 = document.createElement("div");
+        // scoreLi.innerHTML = ("Name : " + initialsDisplay[i] + " Score : " + scoreDisplay[i]);
+        
+        //Set Attributes
+        scoreLi.setAttribute("class", "list-group-item text-left");
+        rowDiv.setAttribute("class", "row");
+        col1.setAttribute("class", "col text-center");
+        col2.setAttribute("class", "col text-center");        
+
+        //set and append
+        col1.innerHTML = ("Name : " + initialsDisplay[i]);
+        col2.innerHTML = ("Score : " + scoreDisplay[i]);
+
         listContainer.append(scoreLi);
+        scoreLi.append(rowDiv);
+        rowDiv.append(col1);
+        rowDiv.append(col2);
         
     }
 }
