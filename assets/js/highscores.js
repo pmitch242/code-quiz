45739b85e92b286853b838953ff724454cd6f5d4
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2019-11-11 04:36:52 -0500

    added clrear localstorage button

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -2,9 +2,11 @@
 var body = document.body;
 var main = document.querySelector(".main");
 var listContainer = document.querySelector(".listcontainer");
+var liElement = document.querySelector("li");
 
 // Elements created
 var backButton = document.createElement("button");
+var clearButton = document.createElement("button");
 
 
 //Functions
@@ -14,13 +16,17 @@ function replacePage() {
 
 // Set Elements
 backButton.textContent = "BACK";
+backButton.setAttribute("class", "btn btn-secondary");
 main.appendChild(backButton);
+clearButton.textContent = "CLEAR HIGH SCORE"
+clearButton.setAttribute("class", "btn btn-secondary");
+main.appendChild(clearButton);
 
 scoreDisplay = localStorage.getItem("scores");
 initialsDisplay = localStorage.getItem("initials");
 scoreDisplay = JSON.parse(scoreDisplay);
 initialsDisplay = JSON.parse(initialsDisplay);
-if( scoreDisplay != null){
+if (scoreDisplay != null) {
 
     for (i = 0; i < scoreDisplay.length; i++) {
         //create Elements
@@ -29,12 +35,12 @@ if( scoreDisplay != null){
         var col1 = document.createElement("div");
         var col2 = document.createElement("div");
         // scoreLi.innerHTML = ("Name : " + initialsDisplay[i] + " Score : " + scoreDisplay[i]);
-        
+
         //Set Attributes
         scoreLi.setAttribute("class", "list-group-item text-left");
         rowDiv.setAttribute("class", "row");
         col1.setAttribute("class", "col text-center");
-        col2.setAttribute("class", "col text-center");        
+        col2.setAttribute("class", "col text-center");
 
         //set and append
         col1.innerHTML = ("Name : " + initialsDisplay[i]);
@@ -44,10 +50,17 @@ if( scoreDisplay != null){
         scoreLi.append(rowDiv);
         rowDiv.append(col1);
         rowDiv.append(col2);
-        
+
     }
 }
 
 backButton.onclick = function () {
     replacePage();
 }
+
+clearButton.addEventListener('click', function () {
+    localStorage.clear();
+    while (scoreLi.firstChild) {
+        scoreLi.removeChild(scoreLi.firstChild)
+    }
+})
